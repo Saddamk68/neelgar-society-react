d876848c6e6cab241886a6f2cb170a99adb74480
Saddamk68 <saddamk68@gmail.com>
2025-09-14 12:41:31 +0530

    feat: impelenting members view specific skeleton

--- src/components/skeletons/MembersSkeleton.tsx
+++ src/components/skeletons/MembersSkeleton.tsx
@@ -1,25 +1,12 @@
-import React from "react";
-import { SkeletonBox } from "./generic/Primitive"; 
-import { ListSkeleton } from "./generic/ListSkeleton";
+import TableSkeleton from "./generic/TableSkeleton";
+import { MEMBER_COLUMNS } from "../../features/members/tableConfig";
 
 export default function MembersSkeleton() {
   return (
-    <div className="p-6 space-y-6">
-      {/* Page header */}
-      <div className="flex justify-between items-center">
-        <SkeletonBox className="h-8 w-40" /> {/* Title */}
-        <SkeletonBox className="h-10 w-32" /> {/* Add Member button */}
-      </div>
-
-      {/* Search/filter bar */}
-      <div className="flex gap-4">
-        <SkeletonBox className="h-10 w-64" />
-        <SkeletonBox className="h-10 w-32" />
-      </div>
-
-      {/* Members list/table */}
-      <div className="bg-white rounded-lg shadow p-4">
-        <ListSkeleton rows={8} />
+    <div className="space-y-4">
+      {/* Table */}
+      <div className="bg-white rounded-xl shadow p-4">
+        <TableSkeleton columns={MEMBER_COLUMNS} rows={8} />
       </div>
     </div>
   );
